c83cf1b583dac70a32fc77cbb8da858332ee49c5
SamuelStrand <105794723+SamuelStrand@users.noreply.github.com>
2024-02-03 15:47:04 +0600

    updated part of 3rd lab

---
+++ labs/lab3/functions1.py
@@ -0,0 +1,24 @@
+# # Task 1
+# def task1(g: float, k=28.349523):
+#     ounces = g * k
+#     return ounces
+#
+#
+# g = float(input())
+# print(task1(g))
+
+# # Task 2
+# def task2(f:float):
+#     c = (5 / 9) * (f - 32)
+#     return c
+#
+# f = int(input())
+# print(task2(f))
+
+# # Task 3
+# def task3(numheads:int, numlegs:int):
+#     r = (numlegs - 2 * numheads) / 2
+#     c = numheads - r
+#     return r, c
+#
+# print(task3(35, 94))
